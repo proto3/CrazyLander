22c2b47367ebbf50b7de6455c427c86a7f3b254b
Antoine <gervail.antoine@gmail.com>
2016-09-23 15:01:18 +0200

    detector working and pose estimator

--- ros_ws/src/aruco_detector/src/aruco_detector_node.cpp
+++ ros_ws/src/aruco_detector/src/aruco_detector_node.cpp
@@ -1,52 +1,66 @@
 #include "ros/ros.h"
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/aruco.hpp>
+#include <geometry_msgs/Vector3.h>
 
-cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-cv::aruco::DetectorParameters detectorParams;
-std::vector<int> ids;
-std::vector<std::vector<cv::Point2f> > corners, rejected;
+#include <vector>
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+int main(int argc, char* argv[])
 {
-    ROS_INFO("Image received.");
 
-    try
-    {
-        cv::Mat img = cv_bridge::toCvShare(msg, "bgr8")->image;
-        cv::aruco::detectMarkers(img, dictionary, corners, ids, detectorParams, rejected);
-        cv::aruco::drawDetectedMarkers(img, corners, ids);
-        if(ids.size() > 0)
-        {
-            //publish marker position
-            //std_msgs::String msg;
-            //position_pub.publish(msg);
-        }
-        cv::imshow("aruco", img);
-        cv::waitKey(30);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-    }
-}
-
-int main(int argc, char **argv)
-{
-    dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
-
-    ros::init(argc, argv, "aruco_detector");
+	ros::init(argc, argv, "aruco_detector");
     ros::NodeHandle n;
+	ros::Publisher vector_pub = n.advertise<geometry_msgs::Vector3>("aruco_pos", 1000);
 
-    cv::namedWindow("aruco");
-    cv::startWindowThread();
-    image_transport::ImageTransport it(n);
-//    ros::Publisher position_pub = n.advertise<std_msgs::String>("aruco_position", 1);
-    image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
-    ros::spin();
-    cv::destroyWindow("aruco");
+    cv::Mat img, imageCopy;
+    cv::VideoCapture *camera = new cv::VideoCapture(1);
+	cv::Mat cameraMatrix;
+	cv::Mat cameraCoeff;
+
+	double cam_tab[9] = {7.4094821037933866e+02, 0., 3.1950000000000000e+02,
+                        0., 7.4094821037933866e+02, 2.3950000000000000e+02,
+                        0., 0., 1.};
+	double coeff[5] = {1.2666795176764284e-01, -1.0966937220820303e+00, 0., 0., 2.8112317830722389e+00};
+	cameraMatrix = cv::Mat(3, 3, CV_64FC1, cam_tab);
+	cameraCoeff = cv::Mat(1,5, CV_64FC1, coeff);
+
+
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+    cv::aruco::DetectorParameters detectorParams;
+    std::vector<int> ids;
+    std::vector<std::vector<cv::Point2f> > corners, rejected;
+	geometry_msgs::Vector3 aruco_pos;
+
+
+	 std::vector< cv::Vec3d > rvecs, tvecs;
+
+
+    while(true)
+    {
+        camera->read(img);
+		img.copyTo(imageCopy);
+        cv::aruco::detectMarkers(img, dictionary, corners, ids, detectorParams, rejected);
+
+		if(ids.size() > 0){
+		//ROS_INFO("img %d", ids.size());
+		//ROS_INFO("corners %f", corners[0][0].x);
+ 			cv::aruco::estimatePoseSingleMarkers(corners, 0.04, cameraMatrix, cameraCoeff, rvecs, tvecs);
+			for(int i=0; i<ids.size(); i++)
+				cv::aruco::drawAxis(imageCopy, cameraMatrix, cameraCoeff, rvecs[i], tvecs[i], 0.1);
+			ROS_INFO("tvecs %lf / %lf / %lf ", tvecs[0][0], tvecs[0][1], tvecs[0][2]);
+			//cv::aruco::drawDetectedMarkers(imageCopy, corners, ids);
+//      	  cv::aruco::drawDetectedMarkers(img, corners, ids, rvecs, tvecs);
+		}
+		aruco_pos.x = 65;
+		aruco_pos.y = 8;
+		aruco_pos.z = 7;
+		vector_pub.publish(aruco_pos);
+        cv::imshow("crazylander", imageCopy);
+        if(cv::waitKey(30) >= 0)
+            break;
+    }
+
+    return 0;
 }
